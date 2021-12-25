4954731c49e8a393d990dd1fc3969bee0c3100fb
Amin Arshadinia <a.arshadinia@novin.dev>
2021-12-25 11:36:25 +0330

    feat : Sidebar component and Compose Button created

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import { Button } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/Inbox';
+
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='sidebar'>
+      <Button className='sidebar__compose' >
+        <img className='sidebar__image' src='https://www.gstatic.com/images/icons/material/colored_icons/1x/create_32dp.png' />
+        Compose
+        </Button>
+        
+        <SidebarOption Icon={InboxIcon} title='Inbox' />
     </div>
   )
 }
