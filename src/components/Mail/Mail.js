4fed5db05feeba0a14a2298f15bf64be481972ef
arshadinia <aminarshadinia@gmail.com>
2021-12-26 09:11:13 +0330

    fix : wrong spelling fixed

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -3,7 +3,7 @@ import './Mail.css'
 import { IconButton } from '@material-ui/core'
 import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import ErorrIcon from '@material-ui/icons/Erorr';
+import ErrorIcon from '@material-ui/icons/Error';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import EmailIcon from '@material-ui/icons/Email';
@@ -18,7 +18,7 @@ const Mail = () => {
         <div className='mail__toolsLeft'>
           <IconButton><ArrowBackIcon /></IconButton>
           <IconButton><MoveToInboxIcon /></IconButton>
-          <IconButton><ErorrIcon /></IconButton>
+          <IconButton><ErrorIcon /></IconButton>
           <IconButton><DeleteIcon /></IconButton>
           <IconButton><EmailIcon /></IconButton>
           <IconButton><WatchLaterIcon /></IconButton>
